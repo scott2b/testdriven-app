0f4fb96f42a2134f223436ad454a505b94710c09
Scott Bradley <scottbradley.mail@gmail.com>
2018-06-26 10:58:55 -0500

    beginning of part3

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -3,11 +3,13 @@ from flask import Flask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_debugtoolbar import DebugToolbarExtension
+from flask_migrate import Migrate
 
 
 # instantiate the db
 db = SQLAlchemy()
 toolbar = DebugToolbarExtension()
+migrate = Migrate()
 
 
 def create_app(script_info=None):
@@ -23,6 +25,7 @@ def create_app(script_info=None):
     # set up extensions
     db.init_app(app)
     toolbar.init_app(app)
+    migrate.init_app(app, db)
 
     # register blueprints
     from project.api.users import users_blueprint
